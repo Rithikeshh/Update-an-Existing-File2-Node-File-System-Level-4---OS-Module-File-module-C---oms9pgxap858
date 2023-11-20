3f8a11faabfb664cc17b8552a47db8fca2acf57d
Rithikeshh <135729296+Rithikeshh@users.noreply.github.com>
2023-11-20 14:58:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ const fileName = 'myfile.txt';
 const updateFile = async (fileName, fileContent) => {
     //Write your code here to overwrite the file content
     //Don't change function name
-    
+    try{
+        const result = await fs.writeFile(fileName, fileContent);
+    } catch(e){
+        console.log(e);
+    }
 };
 
 
